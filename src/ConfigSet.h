d00879b59289dfb34a1cabb0eb6b8f1e4863f77c
Phil B <info@commandserver.org>
2022-06-14 01:01:34 +0200

    seperated slash commands in files & removed using std namespace

--- src/ConfigSet.h
+++ src/ConfigSet.h
@@ -1,7 +1,6 @@
+#pragma once
 #include <dpp/dpp.h>
 #include <set>
-
-using namespace std;
 
 /**
  * Configuration list of unique strings. Saved in a raw txt file.
@@ -9,44 +8,44 @@ using namespace std;
  */
 class ConfigSet {
 private:
-	string file;
-	set<string> list;
-	shared_mutex mutex;
+	std::string file;
+	std::set<std::string> list;
+	std::shared_mutex mutex;
 public:
 	/**
 	 * @param file The filename
 	 */
-	ConfigSet(const basic_string<char> &file);
+	ConfigSet(const std::basic_string<char> &file);
 
 	/**
 	 * Inserts a string to the set unless it already exists
 	 * @param s The string to insert
 	 */
-	void insert(const basic_string<char> &s);
+	void insert(const std::basic_string<char> &s);
 
 	/**
 	 * Check if the set contains a value
 	 * @param s The value to check
 	 * @return
 	 */
-	bool contains(const basic_string<char> &s);
+	bool contains(const std::basic_string<char> &s);
 
 	/**
 	 * Remove a string from the set
 	 * @param s The string to remove
 	 */
-	void remove(const basic_string<char> &s);
+	void remove(const std::basic_string<char> &s);
 
 	/**
 	 * Use get_mutex to protect the container with a shared_mutex
 	 * @return The reference to the internal string set
 	 */
-	set<string> &get_container();
+	std::set<std::string> &get_container();
 
 	/**
 	 * @return The mutex of the set
 	 */
-	shared_mutex &get_mutex();
+	std::shared_mutex &get_mutex();
 
 	/**
 	 * Write the set to the file
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,12 +1,11 @@
+#pragma once
 #include <dpp/dpp.h>
-
-using namespace std;
 
 /**
  * Case Sensitive Implementation of endsWith()
  * It checks if the string 'mainStr' ends with given string 'toMatch'
  */
-bool endsWith(const string &mainStr, const string &toMatch) {
+bool endsWith(const std::string &mainStr, const std::string &toMatch) {
 	if (mainStr.size() >= toMatch.size() &&
 		mainStr.compare(mainStr.size() - toMatch.size(), toMatch.size(), toMatch) == 0)
 		return true;
@@ -20,7 +19,7 @@ bool endsWith(const string &mainStr, const string &toMatch) {
  * @param seconds The amount of seconds to create a time-string for
  * @return The formatted time-string
  */
-string stringifySeconds(uint32_t seconds) {
+std::string stringifySeconds(uint32_t seconds) {
 	uint32_t days = 0, hours = 0, minutes = 0;
 
 	while (seconds >= 86400) {
@@ -36,26 +35,26 @@ string stringifySeconds(uint32_t seconds) {
 		minutes++;
 	}
 
-	string result;
+	std::string result;
 	if (days == 1) {
-		result += to_string(days) + " day ";
+		result += std::to_string(days) + " day ";
 	} else if (days > 1) {
-		result += to_string(days) + " days ";
+		result += std::to_string(days) + " days ";
 	}
 	if (hours == 1) {
-		result += to_string(hours) + " hour ";
+		result += std::to_string(hours) + " hour ";
 	} else if (hours > 1) {
-		result += to_string(hours) + " hours ";
+		result += std::to_string(hours) + " hours ";
 	}
 	if (minutes == 1) {
-		result += to_string(minutes) + " minute ";
+		result += std::to_string(minutes) + " minute ";
 	} else if (minutes > 1) {
-		result += to_string(minutes) + " minutes ";
+		result += std::to_string(minutes) + " minutes ";
 	}
 	if (seconds == 1) {
-		result += to_string(seconds) + " second ";
+		result += std::to_string(seconds) + " second ";
 	} else if (seconds > 1) {
-		result += to_string(seconds) + " seconds ";
+		result += std::to_string(seconds) + " seconds ";
 	}
 	return result.erase(result.find_last_not_of(' ') + 1); // remove the ending space
 }
