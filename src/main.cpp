49fb43e0820b1bae4c2ae50bebfa5c01f600ca89
Phil B <info@commandserver.org>
2022-07-26 18:48:25 +0200

    updated docs

--- src/main.cpp
+++ src/main.cpp
@@ -262,10 +262,10 @@ int main() {
 				}
 			}
 
-			/* when more than 12 members joined in the past 60 seconds
-			 * you can adjust this number depending on your guild-size. I think 12 should be enough
+			/* when more than 10 members joined in the past 60 seconds
+			 * you can adjust this number depending on your guild-size
 			 */
-			if (fast_joined_member_count >= 12) {
+			if (fast_joined_member_count >= 10) {
 				if (!first_join) {
 					first_join = time(nullptr);
 				}
