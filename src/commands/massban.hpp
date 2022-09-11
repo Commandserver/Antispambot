44350dec313e8e84551ba34b432aae03e4ca7c50
Phil B <info@commandserver.org>
2022-09-11 21:05:15 +0200

    refactor massban command

--- src/commands/massban.hpp
+++ src/commands/massban.hpp
@@ -1,7 +1,6 @@
 #include <dpp/dpp.h>
 #include <thread>
 
-
 dpp::slashcommand definition_massban() {
 	return dpp::slashcommand()
 			.set_name("massban")
@@ -32,12 +31,12 @@ void handle_massban(dpp::cluster& bot, const dpp::slashcommand_t& event) {
 	{
 		const auto f = guild->members.find(std::get<dpp::snowflake>(event.get_parameter("first")));
 		if (f == guild->members.end()) {
-			event.reply(dpp::message("User in `first` not found").set_flags(dpp::m_ephemeral));
+			event.reply(dpp::message("Member in `first` not found").set_flags(dpp::m_ephemeral));
 			return;
 		}
 		const auto l = guild->members.find(std::get<dpp::snowflake>(event.get_parameter("last")));
 		if (l == guild->members.end()) {
-			event.reply(dpp::message("User in `last` not found").set_flags(dpp::m_ephemeral));
+			event.reply(dpp::message("Member in `last` not found").set_flags(dpp::m_ephemeral));
 			return;
 		}
 		first = &f->second;
@@ -91,105 +90,105 @@ void handle_massban(dpp::cluster& bot, const dpp::slashcommand_t& event) {
 		send();
 	}
 
-	{
-		// send confirm buttons
 
-		auto confirm_component = dpp::component()
-				.set_label("Ban")
-				.set_type(dpp::cot_button)
-				.set_style(dpp::cos_danger);
+	// send confirm buttons
 
-		ButtonHandler::bind(confirm_component, [&bot, users_to_ban, guild_id = event.command.guild_id, channel_id = event.command.channel_id](const dpp::button_click_t &event) {
-			// TODO check permissions
-			event.reply(
-					dpp::message("Mass ban started! Please wait...")
-			);
+	auto confirm_component = dpp::component()
+			.set_label("Massban")
+			.set_type(dpp::cot_button)
+			.set_style(dpp::cos_danger);
 
-			std::thread t([&bot, users_to_ban, guild_id, channel_id](){
-				bot.log(dpp::ll_info, "mass ban startet in new thread with " + std::to_string(users_to_ban.size()) + " users");
-
-				std::set<dpp::snowflake> success;
-				std::set<dpp::snowflake> failures;
-
-				for (const auto& id : users_to_ban) {
-					try {
-						bot.set_audit_reason("mass ban").guild_ban_add_sync(guild_id, id);
-					} catch (dpp::rest_exception &exception) {
-						bot.log(dpp::ll_error, "couldn't mass ban user id " + std::to_string(id));
-						failures.insert(id);
-						continue;
-					}
-					bot.log(dpp::ll_info, "mass banned user id " + std::to_string(id));
-					success.insert(id);
-				}
-
-				bot.log(dpp::ll_info, "mass ban finished");
-				bot.log(dpp::ll_info, fmt::format("failed to mass ban {} users", failures.size()));
-				bot.log(dpp::ll_info, fmt::format("success mass banned {} users", success.size()));
-
-				// create discord response
-				std::string failures_mentions;
-				for (const auto& id : failures) {
-					failures_mentions += fmt::format("<@{}>", id);
-				}
-				dpp::message m;
-				m.set_channel_id(channel_id);
-				m.add_embed(dpp::embed().set_title("Mass ban finished!")
-									.add_field("successfully banned", fmt::format("**{}** / {}", success.size(), users_to_ban.size()), true)
-									.add_field("failures", fmt::format("**{}**\n{}", failures.size(), failures_mentions), true)
-				);
-				bot.message_create_sync(m);
-				// send a file containing all banned user ids
-				auto filesMsg = dpp::message().set_channel_id(channel_id);
-				if (!success.empty()) {
-					std::string users;
-					for (const auto &id: success) {
-						users += fmt::format("{}\n", id);
-					}
-					filesMsg.add_file("banned-users.txt", users);
-				}
-				if (!failures.empty()) {
-					std::string users;
-					for (const auto &id: failures) {
-						users += fmt::format("{}\n", id);
-					}
-					filesMsg.add_file("failed-users.txt", users);
-				}
-				bot.message_create_sync(filesMsg);
-			});
-			t.detach();
-		});
-
-		auto cancel_confirm = dpp::component()
-				.set_label("Cancel")
-				.set_type(dpp::cot_button)
-				.set_style(dpp::cos_secondary);
-
-		ButtonHandler::bind(cancel_confirm, [](const dpp::button_click_t &event) {
-			event.reply(
-					dpp::interaction_response_type::ir_update_message,
-					dpp::message()
-							.set_flags(dpp::m_ephemeral)
-							.set_content("Mass ban canceled")
-			);
-		});
-
-		dpp::message msg;
-		msg.set_content(fmt::format("Ready to ban **{}** members from this server?\nJoined between **{}** and **{}**",
-									users_to_ban.size(),
-									dpp::utility::timestamp(first->joined_at, dpp::utility::tf_short_datetime),
-									dpp::utility::timestamp(last->joined_at, dpp::utility::tf_short_datetime))
+	ButtonHandler::bind(confirm_component, [&bot, users_to_ban, guild_id = event.command.guild_id, channel_id = event.command.channel_id](const dpp::button_click_t &event) {
+		// TODO check permissions
+		event.reply(
+				dpp::message("Mass ban started! Please wait...")
 		);
-		msg.add_component(
-				dpp::component()
-						.add_component(
-								confirm_component
-						).add_component(
-						cancel_confirm
-				)
-		);
-		msg.set_channel_id(event.command.channel_id);
 
-		bot.message_create(msg);
-	}
+		std::thread t([&bot, users_to_ban, guild_id, channel_id](){
+			bot.log(dpp::ll_info, "mass ban startet in new thread with " + std::to_string(users_to_ban.size()) + " users");
+
+			std::set<dpp::snowflake> success;
+			std::set<dpp::snowflake> failures;
+
+			for (const auto& id : users_to_ban) {
+				try {
+					bot.set_audit_reason("mass ban").guild_ban_add_sync(guild_id, id);
+				} catch (dpp::rest_exception &exception) {
+					bot.log(dpp::ll_error, "couldn't mass ban user id " + std::to_string(id));
+					failures.insert(id);
+					continue;
+				}
+				bot.log(dpp::ll_info, "mass banned user id " + std::to_string(id));
+				success.insert(id);
+			}
+
+			bot.log(dpp::ll_info, "mass ban finished");
+			bot.log(dpp::ll_info, fmt::format("failed to mass ban {} users", failures.size()));
+			bot.log(dpp::ll_info, fmt::format("success mass banned {} users", success.size()));
+
+			// create discord response
+			std::string failures_mentions;
+			for (const auto& id : failures) {
+				failures_mentions += fmt::format("<@{}>", id);
+			}
+			dpp::message m;
+			m.set_channel_id(channel_id);
+			m.add_embed(dpp::embed().set_title("Mass ban finished!")
+								.add_field("successfully banned", fmt::format("**{}** / {}", success.size(), users_to_ban.size()), true)
+								.add_field("failures", fmt::format("**{}**\n{}", failures.size(), failures_mentions), true)
+			);
+			bot.message_create_sync(m);
+			// send a file containing all banned user ids
+			auto filesMsg = dpp::message().set_channel_id(channel_id);
+			if (!success.empty()) {
+				std::string users;
+				for (const auto &id: success) {
+					users += fmt::format("{}\n", id);
+				}
+				filesMsg.add_file("banned-users.txt", users);
+			}
+			if (!failures.empty()) {
+				std::string users;
+				for (const auto &id: failures) {
+					users += fmt::format("{}\n", id);
+				}
+				filesMsg.add_file("failed-users.txt", users);
+			}
+			bot.message_create_sync(filesMsg);
+		});
+		t.detach();
+	});
+
+	auto cancel_confirm = dpp::component()
+			.set_label("Cancel")
+			.set_type(dpp::cot_button)
+			.set_style(dpp::cos_secondary);
+
+	ButtonHandler::bind(cancel_confirm, [](const dpp::button_click_t &event) {
+		event.reply(
+				dpp::interaction_response_type::ir_update_message,
+				dpp::message()
+						.set_flags(dpp::m_ephemeral)
+						.set_content("Mass ban canceled")
+		);
+	});
+
+	dpp::message msg;
+	msg.set_content(fmt::format("Ready to ban **{}** members from this server?\nJoined between **{}** and **{}**",
+								users_to_ban.size(),
+								dpp::utility::timestamp(first->joined_at, dpp::utility::tf_short_datetime),
+								dpp::utility::timestamp(last->joined_at, dpp::utility::tf_short_datetime))
+	);
+	msg.add_component(
+			dpp::component()
+					.add_component(
+							confirm_component
+					).add_component(
+					cancel_confirm
+			)
+	);
+	msg.set_channel_id(event.command.channel_id);
+	msg.add_file("members-to-ban.txt", file);
+
+	bot.message_create(msg);
 }
