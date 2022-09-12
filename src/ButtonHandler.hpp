e2de5baadd4b68cef565cd35e3f87eaf182c049d
Phil B <info@commandserver.org>
2022-09-12 21:11:50 +0200

    added creation timestamp to custom_ids of the ButtonHandler

--- src/ButtonHandler.hpp
+++ src/ButtonHandler.hpp
@@ -2,16 +2,14 @@
 
 #include <dpp/dpp.h>
 
-#ifndef COMPONENT_CACHE_DURATION
 #define COMPONENT_CACHE_DURATION 60 * 10
-#endif
+#define COMPONENT_SPACER "###"
 
 /**
  * Holds the callback for a stored component which will be executed later when a component is triggered
  */
-class ButtonContainer {
+struct ButtonContainer {
 	time_t created_at;
-public:
 	std::function<void(const dpp::button_click_t &)> function;
 	bool only_one;
 
@@ -71,6 +69,7 @@ public:
 				ButtonContainer container;
 				container.function = function;
 				container.only_one = only_one;
+				component.custom_id += COMPONENT_SPACER + std::to_string(static_cast<long int>(container.created_at)); // add creation time to the custom_id
 				cachedActions[customIdCounter] = container;
 				customIdAlreadyExists = false; // break
 			}
@@ -82,11 +81,30 @@ public:
      * @param event the dpp::button_click_t event
      */
 	static void call(const dpp::button_click_t &event) {
+		// parse id and creation time
+		uint64_t customId;
+		time_t creationTimestamp;
+		try {
+			std::string id = event.custom_id.substr(0, event.custom_id.find(COMPONENT_SPACER));
+			std::string creation = event.custom_id.substr(
+					event.custom_id.find(COMPONENT_SPACER) + std::strlen(COMPONENT_SPACER),
+					std::string::npos
+			);
+			customId = std::stoul(id);
+			creationTimestamp = std::stol(creation);
+		} catch (std::out_of_range &e) {
+			event.reply(dpp::message("invalid component").set_flags(dpp::m_ephemeral));
+			return;
+		} catch (std::invalid_argument &e) {
+			event.reply(dpp::message("invalid component").set_flags(dpp::m_ephemeral));
+			return;
+		}
+
 		std::unique_lock l(cachedActionsMutex);
 
-		auto existing = cachedActions.find(std::stoull(event.custom_id));
+		auto existing = cachedActions.find(customId);
 
-		if (existing != cachedActions.end()) {
+		if (existing != cachedActions.end() && existing->second.created_at == creationTimestamp) {
 			existing->second.function(event);
 			if (existing->second.only_one) {
 				cachedActions.erase(existing);
