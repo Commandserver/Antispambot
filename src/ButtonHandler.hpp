f042d8e2e066c4fe7cca67c0999110d8cde7f918
Phil B <info@commandserver.org>
2022-10-03 11:28:14 +0200

    added kick button to log messages 2

--- src/ButtonHandler.hpp
+++ src/ButtonHandler.hpp
@@ -2,7 +2,6 @@
 
 #include <dpp/dpp.h>
 
-#define COMPONENT_CACHE_DURATION 60 * 10
 #define CUSTOM_ID_SPACER "###"
 
 /**
@@ -17,14 +16,16 @@ namespace ButtonHandler {
 	struct Session {
 		time_t created_at;
 		std::function<bool(const dpp::button_click_t &)> function;
+		/// Duration in seconds to hold the session in the cache
+		uint16_t cache_duration;
 
-		Session() : created_at(time(nullptr)) {}
+		Session() : created_at(time(nullptr)), cache_duration(10) {}
 
 		/**
 		 * Whether the callback is expired and can be removed from the cache
 		 */
 		bool isExpired() const {
-			return difftime(time(nullptr), created_at) > COMPONENT_CACHE_DURATION;
+			return difftime(time(nullptr), created_at) > cache_duration * 60;
 		}
 	};
 
@@ -55,11 +56,12 @@ namespace ButtonHandler {
 	/**
      * Set a callback to respond to a component. The function will overwrite the custom_id of the component!
      * @param component component to execute the function for when its triggered. The custom_id field gets overwritten.
+     * @param function callback to execute when the component got triggered.
      * Return true if the callback should be removed from the cache after execution.
      * The handler will then no longer respond to the button. Otherwise return false
-     * @param function callback to execute when the component got triggered
+     * @param cache_duration Duration in minutes to hold the component in the cache. Defaults to 10 minutes
      */
-	void bind(dpp::component &component, const std::function<bool(const dpp::button_click_t &)> &function) {
+	void bind(dpp::component &component, const std::function<bool(const dpp::button_click_t &)> &function, uint16_t cache_duration = 10) {
 		clearGarbage();
 
 		std::unique_lock l(cachedSessionsMutex);
@@ -77,6 +79,7 @@ namespace ButtonHandler {
 
 				Session session;
 				session.function = function;
+				session.cache_duration = cache_duration;
 				component.custom_id += CUSTOM_ID_SPACER + std::to_string(static_cast<long int>(session.created_at)); // add creation time to the custom_id
 				cachedSessions[customIdCounter] = session;
 				customIdAlreadyExists = false; // break
